238b479b94883e9ef80ef7f9954ab497c434acff
maya aderka <mayaad0@gmail.com>
2019-06-17 15:17:45 +0300

    task list has crud and css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,42 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(strong_params)
+    redirect_to tasks_path
+  end
+
+  def edit; end
+
+  def update
+    @task.update(strong_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,18 @@ Rails.application.routes.draw do
   # read all
   get 'tasks', to: 'tasks#index'
   # read one
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
   # create
-
+  # 1. display form
+  get 'new', to: 'tasks#new'
+  # 2. post request
+  post 'tasks', to: 'tasks#create'
   # update
-
+  # 1. display form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
   # delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
