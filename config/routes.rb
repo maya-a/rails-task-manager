aab8e2a028349353256003c2dcd24d0ef1141620
maya aderka <mayaad0@gmail.com>
2019-06-17 11:12:08 +0300

    task index done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # read one
+  # create
+
+  # update
+
+  # delete
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
